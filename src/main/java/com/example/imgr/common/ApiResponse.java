402832987470baaa97ada15b1f763e48dd0c4abe
blackparadise0407 <blackparadise0407@gmail.com>
2022-04-20 00:14:02 +0700

    Update ApiResponse

--- src/main/java/com/example/imgr/common/ApiResponse.java
+++ src/main/java/com/example/imgr/common/ApiResponse.java
@@ -9,12 +9,6 @@ public class ApiResponse<T> {
     private T data;
     private String message;
 
-    public ApiResponse(T data, String message) {
-        super();
-        this.data = data;
-        this.message = message;
-    }
-
     public ApiResponse(T data) {
         super();
         this.data = data;
@@ -24,6 +18,11 @@ public class ApiResponse<T> {
     public ApiResponse(String message) {
         super();
         this.data = null;
+        this.message = message;
+    }
+    public ApiResponse(T data, String message) {
+        super();
+        this.data = data;
         this.message = message;
     }
 }
